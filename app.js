fc4eb96783f3d7e20f4e331277766430f76a37d3
Nur Akmal <2183322C@student.tp.edu.sg>
2022-02-23 00:45:49 +0800

    Update app.js

--- app.js
+++ app.js
@@ -19,8 +19,26 @@ const {
 
 // Create a new client
 const firestore = new Firestore();
+var db = [];
 
 async function getAllWebhooks() {
+	db = [];
+	const customersRef = firestore.collection('customers');
+	const snapshot = await customersRef.get();
+	if (snapshot.empty) {
+		console.log('No matching documents.');
+		return;
+	} else {
+		snapshot.forEach(async doc => {
+			console.log(doc.id, '=>', doc.data()["webhook_url"]);
+			db.push({"webhook_url":doc.data()["webhook_url"],"api_key":doc.data()["api_key"]})
+			return;
+		});
+	}
+}
+
+
+async function generateWebhookDB() {
 	var db = [];
 	const customersRef = firestore.collection('customers');
 	const snapshot = await customersRef.get();
@@ -38,11 +56,6 @@ async function getAllWebhooks() {
 	}
 }
 
-// refresh webhook db whenever theres a change in firestore
-function updateWebhookDB() {
-	getAllWebhooks()
-}
-
 async function createWebhooks(db) {
 
 	const res = await firestore.collection('webhooks_db').add({
@@ -51,8 +64,8 @@ async function createWebhooks(db) {
   
   }
 
-updateWebhookDB();
-
+//generateWebhookDB();
+getAllWebhooks();
 
 app.use(bodyParser.urlencoded({
 	extended: false
@@ -63,6 +76,24 @@ app.use(bodyParser.json());
 //courrio get order API
 router.post('/webhook', async (request, response) => {
 console.log("triggered");
+
+	for (var i in db) {
+
+		await axios
+		.post(db["webhook_url"], {
+			"data": response.body
+		})
+		.then(res => {
+
+			response.status(res.status);
+			response.send(res.data);
+		})
+		.catch(error => {
+			console.error(error)
+			response.statusCode = 401;
+			response.send(error);
+		})
+	}
 
 	response.status(200);
 	response.send("triggered");
